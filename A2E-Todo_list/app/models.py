d8521ea43e9593a464d71824eba33118b906e790
shubham kumar tiwari <shbhmtwr19@gmail.com>
2023-07-19 14:34:53 +0530

    Update models.py

--- A2E-Todo_list/app/models.py
+++ A2E-Todo_list/app/models.py
@@ -1,8 +1,23 @@
-from flask_mongoengine import MongoEngine
+from app.database import db
 
-db = MongoEngine()
+class Task:
+    def __init__(self, description, completed=False):
+        self.description = description
+        self.completed = completed
 
-class Task(db.Document):
-    description = db.StringField(required=True)
-    completed = db.BooleanField(default=False)
+    def save(self):
+        db.tasks.insert_one(self.__dict__)
 
+    @staticmethod
+    def find_all():
+        return list(db.tasks.find())
+
+    @staticmethod
+    def find_by_id(task_id):
+        return db.tasks.find_one({"_id": task_id})
+
+    def update(self):
+        db.tasks.update_one({"_id": self._id}, {"$set": self.__dict__})
+
+    def delete(self):
+        db.tasks.delete_one({"_id": self._id})
