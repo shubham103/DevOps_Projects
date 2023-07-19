af3937ba7169570d6375a21fa95b8f97811479d8
shubham kumar tiwari <shbhmtwr19@gmail.com>
2023-07-19 14:34:01 +0530

    Update controllers.py

--- A2E-Todo_list/app/controllers.py
+++ A2E-Todo_list/app/controllers.py
@@ -1,8 +1,9 @@
 from flask import jsonify, request
+from bson import ObjectId
 from app.models import Task
 
 def get_tasks():
-    tasks = Task.objects()
+    tasks = Task.find_all()
     return jsonify(tasks)
 
 def create_task():
@@ -10,24 +11,23 @@ def create_task():
     description = data.get('description')
     task = Task(description=description)
     task.save()
-    return jsonify(task)
+    return jsonify(task.__dict__)
 
 def get_task(task_id):
-    task = Task.objects.get_or_404(id=task_id)
+    task = Task.find_by_id(ObjectId(task_id))
     return jsonify(task)
 
 def update_task(task_id):
-    task = Task.objects.get_or_404(id=task_id)
     data = request.get_json()
     description = data.get('description')
     completed = data.get('completed')
+    task = Task.find_by_id(ObjectId(task_id))
     task.description = description if description is not None else task.description
     task.completed = completed if completed is not None else task.completed
-    task.save()
+    task.update()
     return jsonify(task)
 
 def delete_task(task_id):
-    task = Task.objects.get_or_404(id=task_id)
+    task = Task.find_by_id(ObjectId(task_id))
     task.delete()
     return jsonify({'message': 'Task deleted successfully'})
-
