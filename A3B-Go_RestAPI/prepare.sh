0f1c565a201c26370d898127bbf22436e9dd182e
shubham kumar tiwari <shbhmtwr19@gmail.com>
2023-07-24 16:52:47 +0530

    Update prepare.sh

--- A3B-Go_RestAPI/prepare.sh
+++ A3B-Go_RestAPI/prepare.sh
@@ -1,1 +1,10 @@
+sudo apt update
+sudo apt upgrade -y
+curl -OL https://golang.org/dl/go1.18.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.18.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> .profile
+source .profile
+go verison
 
+sudo apt install postgresql postgresql-contrib -y
+sudo systemctl start postgresql.service
